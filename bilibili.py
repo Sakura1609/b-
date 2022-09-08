487a300466560882bcd49016f8d7e533b2b70d28
Sakura1609 <3078688722@qq.com>
2022-09-08 09:21:25 +0800

    新增了对异常的处理

--- bilibili.py
+++ bilibili.py
@@ -2,10 +2,13 @@
 爬取bilibili
 """
 import time
+import random
 from webbrowser import get
 import requests
 import re
 import json
+
+from scipy import rand
 
 custom_headers = {
     'Accept': '*/*',
@@ -37,35 +40,45 @@ def get_dict(bvid: str) -> dict:
     video_dict = match.group('dict')
     dic = json.loads(video_dict)
     # 强行让你睡眠一秒, 防止进橘子
-    time.sleep(1)
+    time.sleep(random.random())
     return dic
 
 def produce_json(dic: dict) -> dict:
-    data = {
-    'bvid': dic['bvid'],
-    'title': dic['videoData']['title'],
-    'state': dic['videoData']['stat'],
-    'tags': [],
+    try:
+        data = {
+        'bvid': dic['bvid'],
+        'title': dic['videoData']['title'],
+        'state': dic['videoData']['stat'],
+        'tags': [],
+        }
+        for tag in dic['tags']:
+            data['tags'].append(tag['tag_name'])
+    except KeyError as e:
+        data = {
+            'bvid': '',
+            'title': '',
+            'state': '',
+            'tags': [],
+        }
 
-    }
-    for tag in dic['tags']:
-        data['tags'].append(tag['tag_name'])
-
-    # for item in dic['related']:
-    #     data['related'].append({
-    #         'bvid': item['bvid'],
-    #         'title': item['title']
-    #     })
-        
-    # bvList = []
-    # for item in data['related']:
-    #     bvList.append(item['bvid'])
+        # for item in dic['related']:
+        #     data['related'].append({
+        #         'bvid': item['bvid'],
+        #         'title': item['title']
+        #     })
+            
+        # bvList = []
+        # for item in data['related']:
+        #     bvList.append(item['bvid'])
     return data
 
 def get_related(dic: dict) -> set:
     bvList = []
-    for item in dic['related']:
-        bvList.append(item['bvid'])
+    try:
+        for item in dic['related']:
+            bvList.append(item['bvid'])
+    except KeyError as e:
+        pass
     return bvList
 
 # 广度优先遍历进行爬取
@@ -80,14 +93,25 @@ def width(bvid: str):
             bvid = queue.pop()
             passList.add(bvid)
             dic = get_dict(bvid)
-            f.write(json.dumps(produce_json(dic), indent=4, ensure_ascii=False).encode('utf-8'))
+            f.write(json.dumps(produce_json(dic), ensure_ascii=False).encode('utf-8'))
             f.write(',\n'.encode('utf-8'))
             count = count + 1
-            queue.update(set(get_related(dic)).difference(passList))
+            child = set(get_related(dic)).difference(passList)
+            if child is not None:
+                queue.update(child)
 
 def main():
-    source = "BV1kG4y1r7sr"
-    width(source)
+    while True:
+        try:
+            source = "BV1JB4y1s7Dk"
+            width(source)
+        except Exception as ex:
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print('\n'+message)
+            break
+        finally:
+            break
 
     
 
